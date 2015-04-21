ce7340f695113718bb280f968145fd57e83239b8
ecoal95 <ecoal95@gmail.com>
2015-04-21 18:41:32 +0200

    Make it build on not implemented platforms

--- src/platform/not_implemented/mod.rs
+++ src/platform/not_implemented/mod.rs
@@ -1,1 +1,2 @@
-pub use native_gl_context::NativeGLContext;
+mod native_gl_context;
+pub use self::native_gl_context::NativeGLContext;
--- src/platform/not_implemented/native_gl_context.rs
+++ src/platform/not_implemented/native_gl_context.rs
@@ -1,14 +1,14 @@
 use geom::Size2D;
 use NativeGLContextMethods;
 
-pub struct GLContext;
+pub struct NativeGLContext;
 
-impl GLContextMethods for GLContext {
-    fn create_headless(_: Size2D<i32>) -> Result<GLContext, &'static str> {
+impl NativeGLContextMethods for NativeGLContext {
+    fn create_headless(_: Size2D<i32>) -> Result<NativeGLContext, &'static str> {
         Err("Not implemented (yet)")
     }
 
-    fn is_current() {
+    fn is_current(&self) -> bool {
         false
     }
 
