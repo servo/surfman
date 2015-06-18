43816c39c549ef11da69ae65f1b3f4cdaae67f7d
ecoal95 <ecoal95@gmail.com>
2015-06-18 22:50:24 +0200

    android: Fix android interactions with layers' NativeGraphicsMetadata

--- src/lib.rs
+++ src/lib.rs
@@ -46,7 +46,9 @@ mod egl {
     pub type khronos_uint64_t = libc::uint64_t;
     pub type khronos_ssize_t = libc::c_long;
     pub type EGLint = libc::int32_t;
-    pub type EGLNativeDisplayType = *const libc::c_void;
+    // FIXME: https://github.com/servo/servo/pull/6423#issuecomment-113282933
+    // pub type EGLNativeDisplayType = *const libc::c_void;
+    pub type EGLNativeDisplayType = *mut libc::c_void;
     pub type EGLNativePixmapType = *const libc::c_void;
     pub type EGLNativeWindowType = *const libc::c_void;
     pub type NativeDisplayType = EGLNativeDisplayType;
