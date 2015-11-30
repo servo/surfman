830aee5d567d42d8c9b2f4462ec7f8e35d5d6983
Emilio Cobos Álvarez <ecoal95@gmail.com>
2015-11-30 21:35:30 +0100

    platform: Port to egl and generate egl bindings also in linux
    This will help the task of splitting the backend from glx to egl, I
    think someone is doing that :P

--- src/platform/with_egl/native_gl_context.rs
+++ src/platform/with_egl/native_gl_context.rs
@@ -5,26 +5,25 @@ use std::ffi::CString;
 use egl;
 use egl::types::{EGLint, EGLBoolean, EGLDisplay, EGLSurface, EGLConfig, EGLContext};
 
-#[cfg(feature="texture_surface")]
-use layers::platform::surface::NativeDisplay;
-#[cfg(feature="texture_surface")]
-use std::mem;
+pub struct NativeGLContextHandle(pub EGLDisplay, pub EGLSurface);
+unsafe impl Send for NativeGLContextHandle {}
 
 pub struct NativeGLContext {
     native_display: EGLDisplay,
     native_surface: EGLSurface,
     native_context: EGLContext,
+    weak: bool,
 }
 
 impl NativeGLContext {
-    pub fn new(share_context: Option<&NativeGLContext>,
+    pub fn new(share_context: Option<&EGLContext>,
                display: EGLDisplay,
                surface: EGLSurface,
                config: EGLConfig)
         -> Result<NativeGLContext, &'static str> {
 
         let shared = match share_context {
-            Some(ctx) => ctx.as_native_egl_context(),
+            Some(ctx) => *ctx,
             None => egl::NO_CONTEXT as EGLContext,
         };
 
@@ -46,30 +45,30 @@ impl NativeGLContext {
             native_display: display,
             native_surface: surface,
             native_context: ctx,
+            weak: false,
         })
-    }
-
-    #[inline(always)]
-    pub fn as_native_egl_context(&self) -> EGLContext {
-        self.native_context
     }
 }
 
 impl Drop for NativeGLContext {
     fn drop(&mut self) {
         let _ = self.unbind();
-        unsafe {
-            if egl::DestroySurface(self.native_display, self.native_surface) == 0 {
-                debug!("egl::DestroySurface failed");
-            }
-            if egl::DestroyContext(self.native_display, self.native_context) == 0 {
-                debug!("egl::DestroyContext failed");
+        if !self.weak {
+            unsafe {
+                if egl::DestroySurface(self.native_display, self.native_surface) == 0 {
+                    debug!("egl::DestroySurface failed");
+                }
+                if egl::DestroyContext(self.native_display, self.native_context) == 0 {
+                    debug!("egl::DestroyContext failed");
+                }
             }
         }
     }
 }
 
 impl NativeGLContextMethods for NativeGLContext {
+    type Handle = NativeGLContextHandle;
+
     fn get_proc_address(addr: &str) -> *const () {
         unsafe {
             let addr = CString::new(addr.as_bytes()).unwrap().as_ptr();
@@ -80,7 +79,40 @@ impl NativeGLContextMethods for NativeGLContext {
     fn create_headless() -> Result<NativeGLContext, &'static str> {
         // We create a context with a dummy size, we can't rely on a
         // default framebuffer
-        create_pixel_buffer_backed_offscreen_context(Size2D::new(16, 16))
+        create_pixel_buffer_backed_offscreen_context(Size2D::new(16, 16), None)
+    }
+
+    fn create_shared(with: Option<&Self::Handle>) -> Result<NativeGLContext, &'static str> {
+        create_pixel_buffer_backed_offscreen_context(Size2D::new(16, 16), with)
+    }
+
+    fn current_handle() -> Option<Self::Handle> {
+        let native_context = unsafe { egl::GetCurrentContext() };
+        let native_display = unsafe { egl::GetCurrentDisplay() };
+
+        if native_context != egl::NO_CONTEXT && native_display != egl::NO_DISPLAY {
+            Some(NativeGLContextHandle(native_context, native_display))
+        } else {
+            None
+        }
+    }
+
+
+    fn current() -> Option<Self> {
+        if let Some(handle) = Self::current_handle() {
+            let surface = unsafe { egl::GetCurrentSurface(egl::DRAW as EGLint) };
+
+            debug_assert!(surface != egl::NO_SURFACE);
+
+            Some(NativeGLContext {
+                native_context: handle.0,
+                native_display: handle.1,
+                native_surface: surface,
+                weak: true,
+            })
+        } else {
+            None
+        }
     }
 
     #[inline(always)]
@@ -102,6 +134,10 @@ impl NativeGLContextMethods for NativeGLContext {
                 Ok(())
             }
         }
+    }
+
+    fn handle(&self) -> Self::Handle {
+        NativeGLContextHandle(self.native_context, self.native_display)
     }
 
     fn unbind(&self) -> Result<(), &'static str> {
