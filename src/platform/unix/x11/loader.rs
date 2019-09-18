3937149ba30576d548ec7cf75407cf3a6b2bd2c9
Patrick Walton <pcwalton@mimiga.net>
2019-09-18 16:56:58 -0700

    wip

--- src/platform/unix/x11/loader.rs
+++ src/platform/unix/x11/loader.rs
@@ -3,24 +3,26 @@
 use crate::glx;
 
 use gl::types::GLubyte;
-use libc::dlsym;
+use libc::{RTLD_DEFAULT, dlsym};
 use std::ffi::CString;
-use std::os::raw::c_void;
+use std::mem;
+use std::os::raw::{c_char, c_void};
 
 lazy_static! {
-    static ref GLX_GET_PROC_ADDRESS: extern "C" fn(*const GLubyte) -> extern "C" fn() {
+    static ref GLX_GET_PROC_ADDRESS: extern "C" fn(*const GLubyte) -> *mut c_void = {
         unsafe {
-            let function = dlsym(&b"glXGetProcAddress\0"[0]);
+            let symbol = &b"glXGetProcAddress\0"[0] as *const u8 as *const i8;
+            let function = dlsym(RTLD_DEFAULT, symbol);
             assert!(!function.is_null());
-            function
+            mem::transmute(function)
         }
-    }
+    };
 }
 
 pub fn get_proc_address(symbol_name: &str) -> *const c_void {
     unsafe {
         let symbol_name: CString = CString::new(symbol_name).unwrap();
-        (GLX_GET_PROC_ADDRESS.get())(symbol_name.as_ptr() as *const u8)
+        (*GLX_GET_PROC_ADDRESS)(symbol_name.as_ptr() as *const u8) as *const c_void
     }
 }
 
