351f1201b834bab164581a063c843fc81b74f461
Patrick Walton <pcwalton@mimiga.net>
2019-09-27 22:12:36 -0700

    wip

--- surfman/resources/examples/check.fs.glsl
+++ surfman/resources/examples/check.fs.glsl
@@ -11,18 +11,21 @@ in vec2 vTexCoord;
 out vec4 oFragColor;
 
 const float PI = 3.14159;
+const float RADIUS = 96.0;
+const float RADIUS_SQ = RADIUS * RADIUS;
+const vec3 CENTER = vec3(RADIUS, RADIUS, 0.0);
 
 void main() {
     float t = -1.0;
-    vec3 rayOrigin = vec3(0.0, 0.0, -10.0);
-    vec3 rayDirection = normalize(vec3(vTexCoord, 0.0) - rayOrigin);
+    vec3 rayOrigin = vec3(CENTER.xy, -1000.0);
+    vec3 rayDirection = normalize(vec3(gl_FragCoord.xy, 0.0) - rayOrigin);
 
-    vec3 originToCenter = vec3(0.0) - rayOrigin;
+    vec3 originToCenter = CENTER - rayOrigin;
     float tCA = dot(originToCenter, rayDirection);
     if (tCA >= 0.0) {
         float d2 = dot(originToCenter, originToCenter) - tCA * tCA;
-        if (d2 <= 1.0) {
-            float tHC = sqrt(1.0 - d2);
+        if (d2 <= RADIUS_SQ) {
+            float tHC = sqrt(RADIUS_SQ - d2);
             vec2 ts = vec2(tCA) + vec2(-tHC, tHC);
             ts = vec2(min(ts.x, ts.y), max(ts.x, ts.y));
             t = ts.x >= 0.0 ? ts.x : ts.y;
@@ -34,20 +37,20 @@ void main() {
         return;
     }
 
-    vec3 hitPosition = normalize(rayOrigin + rayDirection * vec3(t));
+    vec3 hitPosition = rayOrigin + rayDirection * vec3(t);
+    vec3 normal = normalize(hitPosition - CENTER);
 
-    hitPosition = mat3(vec3(cos(uRotation.y), 0.0, sin(uRotation.y)),
-                       vec3(0.0, 1.0, 0.0),
-                       vec3(-sin(uRotation.y), 0.0, cos(uRotation.y))) * hitPosition;
-    hitPosition = mat3(vec3(1.0, 0.0, 0.0),
-                       vec3(0.0, cos(uRotation.x), -sin(uRotation.x)),
-                       vec3(0.0, sin(uRotation.x),  cos(uRotation.x))) * hitPosition;
-    hitPosition = mat3(vec3(cos(uRotation.z), -sin(uRotation.z), 0.0),
-                       vec3(sin(uRotation.z),  cos(uRotation.z), 0.0),
-                       vec3(0.0, 0.0, 1.0)) * hitPosition;
+    normal = mat3(vec3(cos(uRotation.y), 0.0, sin(uRotation.y)),
+                  vec3(0.0, 1.0, 0.0),
+                  vec3(-sin(uRotation.y), 0.0, cos(uRotation.y))) * normal;
+    normal = mat3(vec3(1.0, 0.0, 0.0),
+                  vec3(0.0, cos(uRotation.x), -sin(uRotation.x)),
+                  vec3(0.0, sin(uRotation.x),  cos(uRotation.x))) * normal;
+    normal = mat3(vec3(cos(uRotation.z), -sin(uRotation.z), 0.0),
+                  vec3(sin(uRotation.z),  cos(uRotation.z), 0.0),
+                  vec3(0.0, 0.0, 1.0)) * normal;
 
-    vec2 uv = vec2((1.0 + atan(hitPosition.z, hitPosition.x) / PI) * 0.5,
-                   acos(hitPosition.y) / PI) * vec2(12.0);
+    vec2 uv = vec2((1.0 + atan(normal.z, normal.x) / PI) * 0.5, acos(normal.y) / PI) * vec2(12.0);
 
     ivec2 on = ivec2(greaterThanEqual(mod(uv, vec2(2.0)), vec2(1.0)));
     oFragColor = ((on.x ^ on.y) > 0) ? uColorA : uColorB;
