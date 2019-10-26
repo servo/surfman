98dd0fa7d0db6e61d80f514a9b3ad149b3b63f3b
Patrick Walton <pcwalton@mimiga.net>
2019-10-26 16:51:13 -0700

    Update connections

--- surfman/src/platform/unix/x11/connection.rs
+++ surfman/src/platform/unix/x11/connection.rs
@@ -8,6 +8,11 @@ use crate::error::Error;
 
 use std::ffi::CString;
 
+#[cfg(feature = "sm-winit")]
+use winit::Window;
+#[cfg(feature = "sm-winit")]
+use winit::os::unix::WindowExt;
+
 #[derive(Clone)]
 pub struct Connection {
     pub(crate) display_name: Option<CString>,
@@ -18,5 +23,10 @@ impl Connection {
     #[inline]
     pub fn new() -> Result<Connection, Error> {
         Ok(Connection { display_name: None })
+    }
+
+    #[cfg(feature = "sm-winit")]
+    pub fn from_winit_window(_: &Window) -> Result<Connection, Error> {
+        Connection::new()
     }
 }
